a189e626e3537ecb90d691543bc35947318408cf
romansaborido <roman.saborido@iesnervion.es>
2025-05-23 14:27:49 +0200

    fixed exercise10

--- Tema7/src/exercises/Ejercicio10.java
+++ Tema7/src/exercises/Ejercicio10.java
@@ -16,11 +16,8 @@ public class Ejercicio10 {
 		// Variable para almacenar el id del curso introducido por el usuario
 		int idCurso;
 		
-		// Creamos la consulta para obtener el id de los estudiantes que cursan dicho curso
-		String sql1 = "SELECT id_estudiante FROM Matriculas WHERE id_curso = ";
-		
-		// Creamos la consulta para obtener toda la tabla
-		String sql2 = "SELECT * FROM Estudiantes WHERE id_estudiante = ";
+		// Variable para almacenar la consulta sql
+		String sql;
 		
 		// Creamos el scanner para obtener el id del curso que introduzca el usuario
 		Scanner reader = new Scanner(System.in);
@@ -29,30 +26,26 @@ public class Ejercicio10 {
 		System.out.println("Introduzca el id del curso");
 		idCurso = reader.nextInt();
 		
+		// Creamos la consulta para obtener los estudiantes del curso con el id introducido por el usuario
+		sql = "SELECT * FROM Estudiantes WHERE id_estudiante IN (SELECT id_estudiante FROM Calificaciones WHERE id_curso = " + idCurso + ")";
+		
 		// Conectamos con la base de datos
 		try (Connection con = DriverManager.getConnection(Creedenciales.URL, Creedenciales.USER, Creedenciales.PASSWORD)) {
 			
 			// Creamos un statement
 			Statement st = con.createStatement();
 			
-			// Seleccionamos el id de los estudiantes
-			ResultSet rs1 = st.executeQuery(sql1 + idCurso);
+			// Obtenemos los estudiantes
+			ResultSet rs = st.executeQuery(sql);
 			
-			// Obtenemos el id de los estudiantes y mostramos unicamente el nombre y la fecha de nacimiento
-			while (rs1.next()) {
-				
-				// Ejecutamos la consulta y mostramos su resultado
-				ResultSet rs2 = st.executeQuery(sql2 + rs1.getString(1));
-				
-				// Mostramos unicamente el nombre y la fecha de nacimiento
-				while (rs2.next()) {
-					System.out.println(rs2.getString(2) + "\t" + rs2.getString(4));
-				}
+			// Mostramos unicamente el nombre y la fecha de nacimiento
+			while (rs.next()) {
+				System.out.println(rs.getString(2) + "\t" + rs.getString(4));
 			}
 				
 		// Capturamos la excepcion y mostramos un mensaje de error
 		} catch (SQLException e) {
-			System.out.println("Ha ocurrido un error al conectar con la base de datos");
+			System.out.println("Ha ocurrido un error en la conexión con la base de datos -> " + e.getMessage());
 		}
 		
 		// Cerramos el scanner
